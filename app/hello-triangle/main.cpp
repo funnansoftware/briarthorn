c305ed5f1ca8e32a154f707c02756a79a2539776
funnan-dev <funnan@funnansoftware.com>
2026-07-06 17:05:54 -0400

    Initial commit.

---
+++ app/hello-triangle/main.cpp
@@ -0,0 +1,23 @@
+#include <print>
+
+#include <raylib.h>
+
+auto main() -> int
+{
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 600, "Hello, Triangle");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(GRAY);
+        DrawTriangle(Vector2{400, 100}, Vector2{300, 300}, Vector2{500, 300}, ORANGE);
+        EndDrawing();
+        PollInputEvents();
+        SwapScreenBuffer();
+    }
+
+    CloseWindow();
+
+    return EXIT_SUCCESS;
+}
